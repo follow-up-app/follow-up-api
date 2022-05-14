414cd13f03546a0199a73e9a004fffe413e54688
Rodrigo Rosa <rodrigo.mrosa@hotmail.com>
2022-05-14 15:58:21 -0300

    init tests implements

---
+++ tests/__init__.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+from starlette.testclient import TestClient
+
+from main import app
+from config import Settings, get_settings
+
+
+def delete_db():
+    os.remove('./test.db')
+
+
+def get_settings_override():
+    return Settings(
+        SQLALCHEMY_DATABASE_URI="sqlite:///./test.db",
+        TESTING=True
+    )
+
+
+app.dependency_overrides[get_settings] = get_settings_override
+
+
+def get_client(token: str = None) -> TestClient:
+    client = TestClient(app)
+    if token:
+        client.headers = {
+            'Authorization': 'Bearer ' + token
+        }
+    return client
+
+
+def run_test_with_try(func):
+    def function_wrapper():
+        print("Starting test " + func.__name__)
+        try:
+            func()
+        except Exception as ex:
+            print(ex)
+            raise ex
+        finally:
+            delete_db()
+        print("Finished test " + func.__name__)
+
+    return function_wrapper

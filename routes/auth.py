7262c6f14dabeff1851ed40f6da8497bb6e13f18
Rodrigo Rosa <rodrigo.mrosa@hotmail.com>
2022-09-26 19:51:22 -0300

    refresh jwt token

--- routes/auth.py
+++ routes/auth.py
@@ -46,6 +46,16 @@ async def login(login_schema_in: LoginSchemaIn,
     )
     return LoginSchemaOut(access_token=access_token, token_type="bearer")
 
+@router.get("/refresh", tags=[tag], summary="Refresh token of logged user", response_model=LoginSchemaOut)
+async def refresh(current_user: User = Depends(get_current_user), settings: Settings = Depends(get_settings)):
+    access_token_expires = timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        settings,
+        data={"sub": current_user.email}, expires_delta=access_token_expires
+    )
+    return LoginSchemaOut(access_token=access_token, token_type="bearer")
+    
 
 @router.get("/me", tags=[tag], summary="Returns data of logged user", response_model=UserOut)
 async def me(current_user: User = Depends(get_current_user)):
